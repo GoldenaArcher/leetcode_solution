4c17bdfc0f2347586e7e5b7f84dfb2b053bd3f50
GoldenaArcher <lu.han9467@gmail.com>
2018-03-26 17:50:12 -0600

    ri

--- src/Math/ReverseInteger.java
+++ src/Math/ReverseInteger.java
@@ -1,4 +1,18 @@
 package Math;
 
 public class ReverseInteger {
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int mod = x % 10;
+            int tempVal = result*10 + mod;
+
+            if (((tempVal - mod) / 10) != result)
+                return 0;
+
+            result = tempVal;
+            x /= 10;
+        }
+        return result;
+    }
 }
